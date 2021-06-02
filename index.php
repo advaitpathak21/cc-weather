45e6e50d9971e8e25e9516eeecf0918a8f2e093d
advaitpathak21 <55303152+advaitpathak21@users.noreply.github.com>
2021-06-02 16:57:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -82,7 +82,7 @@
                 ?>
             </div>
 		</div>		
-		<p id="name" class="text-white">&copy;Advait Pathak</p>
+		<!--<p id="name" class="text-white">&copy;Advait Pathak</p> -->
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
